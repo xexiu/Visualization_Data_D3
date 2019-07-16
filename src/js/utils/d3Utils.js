b7c4b05f0269d5cef3c8f5a66462b543939731e9
smironescu <sergio.mironescu@marfeel.com>
2019-07-16 20:57:42 +0200

    import only necessary stuff from D3 library

--- src/js/utils/d3Utils.js
+++ src/js/utils/d3Utils.js
@@ -1,15 +1,22 @@
 /* eslint-disable padding-line-between-statements */
-import * as d3 from 'd3';
+import {
+	ascending,
+	descending,
+	select,
+	arc,
+	pie,
+	interpolate
+} from 'd3';
 
 const DEFAULT_WIDTH = 50;
 const DEFAUTL_HEIGHT = 50;
 
 export function shortValuesD3(a, b) {
-	return a < b ? d3.ascending(a, b) : d3.descending(a, b);
+	return a < b ? ascending(a, b) : descending(a, b);
 }
 
 export function buildSVG(selector = '', width = DEFAULT_WIDTH, height = DEFAUTL_HEIGHT) {
-	return d3.select(selector)
+	return select(selector)
         .append('svg')
         .attr('width', width)
 		.attr('height', height)
@@ -17,23 +24,23 @@ export function buildSVG(selector = '', width = DEFAULT_WIDTH, height = DEFAUTL_
 }
 
 export function buildArc(innerRadius = DEFAULT_WIDTH, outerRadius = DEFAUTL_HEIGHT) {
-	return d3.arc()
+	return arc()
             .innerRadius(innerRadius)
             .outerRadius(outerRadius);
 }
 
 export function buildPie() {
-	return d3.pie()
+	return pie()
 		.sort(shortValuesD3)
 		.value(d => d);
 }
 
-export function interpolateAngles(arc, d) {
+export function interpolateAngles(_arc, d) {
 	const dimension = d;
-	const i = d3.interpolate(dimension.startAngle + 0.1, dimension.endAngle);
+	const i = interpolate(dimension.startAngle + 0.1, dimension.endAngle);
 
 	return (t) => {
 		dimension.endAngle = i(t);
-		return arc(dimension);
+		return _arc(dimension);
 	};
 }
